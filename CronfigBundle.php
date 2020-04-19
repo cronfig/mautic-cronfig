404c02bf19010bf5251f66a08185f07f211ddd5d
John Linhart <admin@escope.cz>
2020-04-19 14:20:04 +0200

    CS fix

--- CronfigBundle.php
+++ CronfigBundle.php
@@ -3,7 +3,7 @@
  * @copyright   2016 Cronfig.io. All rights reserved
  * @author      Jan Linhart
  *
- * @link        http://cronfig.io
+ * @see         http://cronfig.io
  *
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -12,9 +12,6 @@ namespace MauticPlugin\CronfigBundle;
 
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
 
-/**
- * Class CronfigBundle.
- */
 class CronfigBundle extends PluginBundleBase
 {
 }
